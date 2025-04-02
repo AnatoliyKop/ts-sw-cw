e6070b0ba1212e8e94b1ae8c5234184f7daabc68
AnatoliyKop <ttolea90tolea132@gmail.com>
2025-04-02 17:42:12 +0200

    Header change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,16 @@
 import Navigation from "./Navigation.tsx";
+import {characters, defaultHero} from "../utils/constants.ts";
+import {useParams} from "react-router";
 
 const Header = () => {
+    const {heroId = defaultHero} = useParams();
+    const heroName = characters[heroId as keyof typeof characters].name || characters[defaultHero].name;
+    console.log(heroName);
+
     return (
         <header className="rounded-top-4">
             <Navigation/>
-            <h1 className="text-center fs-1 py-4">Luke Skywalker</h1>
+            <h1 className="text-center fs-1 py-4">{heroName} +1</h1>
         </header>
     );
 };
